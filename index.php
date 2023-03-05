72c3050151f74d82d4526288f55acd87df83ac91
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-05 13:15:03 +0800

    Update files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+    require dirname(__FILE__) . '/main/head.php'; 
+    require dirname(__FILE__) . '/main/header.php';
+?>
+    index
+<?php
+    require dirname(__FILE__) . '/main/footer.php';
+?>
