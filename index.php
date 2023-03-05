54a3e04aaec6f1e2ff5231221aad376e0839842c
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-05 14:53:03 +0800

    update test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require dirname(__FILE__) . '/main/head.php'; 
     require dirname(__FILE__) . '/main/header.php';
 ?>
-    index
+    <h1>Dit is h1</h1>
     <?= $_SESSION['path'] ?>
 <?php
     require dirname(__FILE__) . '/main/footer.php';
