4999fbdefb52609818f8ee69155398e4922dbb7c
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-05 14:32:28 +0800

    update mappen

--- main/head.php
+++ main/head.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <?php
+        session_start();
+        $_SESSION['path'] = dirname(__FILE__);
+    ?>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portofolio</title>
+    
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= $_SESSION['path'] ?>/images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= $_SESSION['path'] ?>/images/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= $_SESSION['path'] ?>/images/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?= $_SESSION['path'] ?>/images/favicons/site.webmanifest">
+
+    <title>WebDaan</title>
 
     <!-- Bootstrap link -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <?php
-        session_start();
-        $_SESSION["path"] = __DIR__;
-    ?>
+    
 </head>
 <body>
