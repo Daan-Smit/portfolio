2fcb26136bfd743e20912a1f46c9684731fdda20
Daan-Smit <daan.smit@kpnmail.nl>
2023-03-08 09:45:26 +0800

    test

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 ?>
     <h1>Dit is h1</h1>
     <?= $_SESSION['path'] ?>
+    <img src="<?= $_SESSION['path'] . '/images/logos/color-no-bg.png' ?>" alt="">
 <?php
     require dirname(__FILE__) . '/main/footer.php';
 ?>
